e56b700b6f3cfe409009ca4a6017fb42999eeec4
EclipzWasTaken <143146713+EclipzWasTaken@users.noreply.github.com>
2025-08-07 03:03:17 -0400

    Update video generation and assets for MeetYourFamily
    Replaced audio track with 'Brother.mp3', updated image folder structure and naming, improved text overlays, and added a new font file. Removed temporary video files and updated the main script to enhance video composition and audio integration.

--- MeetYourFamily.py
+++ MeetYourFamily.py
@@ -1,4 +1,4 @@
-from moviepy import VideoFileClip, ImageClip, TextClip, CompositeVideoClip, concatenate_videoclips
+from moviepy import VideoFileClip, ImageClip, TextClip, CompositeVideoClip, concatenate_videoclips, AudioFileClip
 import random
 
 # Base video path
@@ -13,22 +13,26 @@ Friend = ['Tate', 'Speed', 'Ross', 'NLE', 'Hart', 'Druski', 'Cenat', 'Black']
 Aunty_Daughter = ['SZA', 'Stallion', 'Spice', 'Minaj']
 
 
-list_of_titles = [['Dad', Dad], ['Mom', Mom], ['Aunt', Aunt], ['Little SIs', Little_Sis], ['Big Sis', Big_sis], ['Friend', Friend], ["Aunty's Daughter", Aunty_Daughter]]
+list_of_titles = [['DAD', Dad], ['MOM', Mom], ['AUNT', Aunt], ['LITTLE SIS', Little_Sis], ['BIG SIS', Big_sis], ['FRIEND', Friend], ["Mi Prima", Aunty_Daughter]]
 
 # Load base video
 base_clip = VideoFileClip(base_video_path)
 
 # take the first 13 seconds of the base video
-base_clip = base_clip.with_duration(13).resized(height=1920, width=1080)
+base_clip = base_clip.with_duration(13).resized(height=1920, width=1080).without_audio()
+
+# Add text to the base video
+base_text = TextClip(text="Meet Your Family", font_size=90, color='blue', font= 'font.ttf', size = (340, 500)).with_duration(13).with_position(("center", 500))
+base_clip = CompositeVideoClip([base_clip, base_text])
 
 clips = [base_clip]
 
 # Create text clip for the member's name
 for title in list_of_titles:
-    text_clip = TextClip(text = title[0], font_size=20, color='white', font='font.ttf').with_duration(5)
-    text_clip = text_clip.with_position(("center", 50))
+    text_clip = TextClip(text = title[0], font_size=50, color='blue', font= 'font.ttf', size = (340, 500)).with_duration(5)
+    text_clip = text_clip.with_position(('center', 200))
     person_name = random.choice(title[1])
-    person_clip = ImageClip(f"images/{title[0].lower()}/{person_name.lower()}.jpg").with_duration(5).with_position("center").resized(height=200, width=400)
+    person_clip = ImageClip(f"images/{title[0].lower()}/{person_name.lower()}.jpg").with_duration(5).with_position("center").resized(height=800)
     person_clip = person_clip.with_position(("center", "center"))
     # Add the text and person clip to the base video
     final_clip = CompositeVideoClip([person_clip, text_clip])
@@ -36,8 +40,11 @@ for title in list_of_titles:
     #base_clip = concatenate_videoclips([base_clip, person_clip, text_clip])
     clips.append(final_clip)
 
+audio = AudioFileClip('Brother.mp3')
+
+
 # Concatenate all clips into a final video
 final_video = concatenate_videoclips(clips, method="compose")
-
+final_video = final_video.with_audio(audio) # Add audio to the final video
 # Save the final video
 final_video.write_videofile("MeetYourFamily.mp4", fps=24)
